25388fd8f349cbe021a3a3fcb8f4e95de89cf6d4
straxFromIbr <haga.yuya2.2016@gmail.com>
2021-11-09 12:01:56 +0900

    学習時のコールバックやユーティリティーをフォルダニまとめた

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -1,35 +1,10 @@
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import tensorflow.keras as keras
+
+from .display import create_mask
+
 from IPython.display import clear_output
-
-
-def generate_images(test_input, tar):
-    plt.figure(figsize=(15, 15))
-
-    display_list = [test_input[0], tar[0]]
-    title = ["Input Image", "Ground Truth"]
-
-    for i in range(2):
-        plt.subplot(1, 2, i + 1)
-        plt.title(title[i])
-        # Getting the pixel values in the [0, 1] range to plot.
-        plt.imshow(display_list[i] * 0.5 + 0.5)
-        plt.axis("off")
-    plt.show()
-
-
-def create_mask(pred_mask):
-    pred_mask = tf.argmax(pred_mask, axis=-1)
-    pred_mask = pred_mask[..., tf.newaxis]
-    return pred_mask[0]
-
-
-def show_predictions(model, dataset=None, num=1):
-    if dataset:
-        for image, mask in dataset.take(num):
-            pred_mask = model.predict(image)
-            generate_images(image[0], create_mask(pred_mask))
 
 
 class DisplayCallback(keras.callbacks.Callback):
---
+++ utils/display.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+
+def create_mask(pred_mask):
+    pred_mask = tf.argmax(pred_mask, axis=-1)
+    pred_mask = pred_mask[..., tf.newaxis]
+    return pred_mask[0]
