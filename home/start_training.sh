04b5fe3ea7f7d36295e8eb2ae3ce32bb0f9182b5
straxFromIbr <haga.yuya2.2016@gmail.com>
2021-11-16 13:18:09 +0900

    引数で使用する損失関数を決める

--- home/start_training.sh
+++ home/start_training.sh
@@ -3,7 +3,9 @@ set -euxo pipefail
 cd "$(dirname "$0")"
 
 tensorboard --bind_all --logdir ./logs &
-python3 ./train.py
+python3 ./train.py TVERSKY
+python3 ./train.py DICE
+python3 ./train.py FOCAL
 pkill tensorboard
 tensorboard dev upload --logdir ./logs --one_shot
 
