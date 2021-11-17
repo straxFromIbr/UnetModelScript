ed724198cdf1ccb0bad4381668013f40da83f09c
straxFromIbr <haga.yuya2.2016@gmail.com>
2021-11-17 13:25:29 +0900

    奇跡的に動いている

--- home/train.py
+++ home/train.py
@@ -1,60 +1,53 @@
-# %%
 import sys
-from datetime import datetime
 
 import tensorflow.keras as keras
 
-from model import unet, losses, metrics
-
 import config
+from model import unet, losses, metrics
 from utils import callbacks
 from dataset_utils import mk_dataset
 
 
 # %%
+# Get Datasets
 def make_datasets():
-    train_ds = mk_dataset.mk_dataset(
-        SAT_PATH=config.TR_SAT_PATH,
-        MAP_PATH=config.TR_MAP_PATH,
-    )
-    valid_ds = mk_dataset.mk_dataset(
-        SAT_PATH=config.VA_SAT_PATH,
-        MAP_PATH=config.VA_MAP_PATH,
-    )
+    train_ds = mk_dataset.mk_base_dataset(config.TR_SAT_PATH, config.TR_MAP_PATH)
+    train_ds = mk_dataset.augument_ds(train_ds)
+    train_ds = mk_dataset.post_process_ds(train_ds)
+
+    valid_ds = mk_dataset.mk_base_dataset(config.VA_SAT_PATH, config.VA_MAP_PATH)
+    valid_ds = mk_dataset.post_process_ds(valid_ds)
     return train_ds, valid_ds
 
 
 # %%
-# Define Model
-def compile_model(loss, optimizer):
-    input_shape = (config.IMG_HEIGHT, config.IMG_WIDTH, config.IMG_CH)
+# Define model
+def compile_model(loss):
     model = unet.big_unet_model(
-        input_shape=input_shape,
+        input_shape=config.INPUT_SIZE,
         output_channels=config.OUT_CH,
     )
     # Compile the model
+    optimizer = keras.optimizers.Adam()
     metric_list = ["accuracy", metrics.iou_coef]
     model.compile(optimizer=optimizer, loss=loss, metrics=metric_list)
     return model
 
 
 # %%
-# Show Model shape
-
-
+# Define Callbacks
 def get_callbacks(filename):
     tboard_cb = callbacks.get_tboard_callback(str(config.LOG_PATH / filename))
     checkpoint_cb = callbacks.get_checkpoint_callback(
-        str(config.CHECKPOINT_PATH / filename)
+        str(config.CHECKPOINT_PATH / filename / filename)
     )
     callback_list = [tboard_cb, checkpoint_cb]
     return callback_list
 
 
 # %%
-def train(train_ds, valid_ds, NB_EPOCHS, loss, optimizer=keras.optimizers.Adam()):
-    model = compile_model(loss=loss, optimizer=optimizer)
-    filename = datetime.now().strftime("%y%m%d%H_") + model.loss.name
+def train(model: keras.Model, train_ds, valid_ds, NB_EPOCHS):
+    filename = model.loss.name
     model_history = model.fit(
         train_ds,
         epochs=NB_EPOCHS,
@@ -68,22 +61,23 @@ def train(train_ds, valid_ds, NB_EPOCHS, loss, optimizer=keras.optimizers.Adam()
 
 
 # %%
-def main():
-    lossfunc_dict = {
-        "DICE": losses.DICELoss(name="DICE"),
-        "FOCAL": losses.FocalTverskyLoss("Focal"),
-        "TVERSKY": losses.TverskyLoss("Tversky"),
-    }
+
+if __name__ == "__main__":
     train_ds, valid_ds = make_datasets()
-    loss = lossfunc_dict[sys.argv[1]]
+    if sys.argv[1] == "Tversky":
+        loss = losses.TverskyLoss(name="Tversky")
+    elif sys.argv[1] == "DICE":
+        loss = losses.DICELoss(name="DICE")
+    elif sys.argv[1] == "Focal":
+        loss = losses.FocalTverskyLoss(name="Focal")
+    else:
+        print("requires one argument: loss")
+        sys.exit(1)
+
+    model = compile_model(loss=loss)
     hist = train(
+        model=model,
         train_ds=train_ds,
         valid_ds=valid_ds,
-        NB_EPOCHS=500,
-        loss=loss,
+        NB_EPOCHS=10,
     )
-
-
-# %%
-if __name__ == "__main__":
-    main()
