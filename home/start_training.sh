4b51d25921ee67a05c595208ac8665afe2c46dd5
straxFromIbr <haga.yuya2.2016@gmail.com>
2021-11-26 16:07:19 +0900

    学習スクリプトバックアップ

--- home/start_training.sh
+++ home/start_training.sh
@@ -2,10 +2,7 @@
 set -euxo pipefail
 cd "$(dirname "$0")"
 
-tensorboard dev upload --logdir ./logs --name '実験' --description 'CutMixはOFFに、TverskyのAlpha値を変えて試してみる' &
-python3 ./train.py --logdir 'cmF-a3-E20' --alpha 0.3 --epochs 50
-python3 ./train.py --logdir 'cmF-a5-E20' --alpha 0.5 --epochs 50
-python3 ./train.py --logdir 'cmF-a7-E20' --alpha 0.7 --epochs 50
-pkill tensorboard
-#tensorboard dev upload --logdir ./logs --one_shot
+python3 ./train.py --logdir 'cmTn3-a7-E20_MR' --use_cutmix --alpha 0.7 --epochs 20
+#python3 ./train.py --logdir 'cmF-a5-E20' --alpha 0.5 --epochs 50
+#python3 ./train.py --logdir 'cmF-a7-E20' --alpha 0.7 --epochs 50
 
